9044a2bfc522315c08dbc24654340bcf19da35ce
BarefootElf <AndreiRoot@protonmail.ch>
2023-01-27 16:52:43 +0300

    Add default mvc project

---
+++ ITStore/ITStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
